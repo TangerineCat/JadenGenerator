4e3016a08def763a11c4f8608df7da6a5c2e90de
Kevin Tang <ktang@caltech.edu>
2015-02-19 06:50:59 -0800

    Removed keys from download file
    use your own keys to keep

---
+++ JaydenDownload.py
@@ -0,0 +1,20 @@
+import twitter
+import json
+
+CONSUMER_KEY = 'XXX'
+CONSUMER_SECRET = 'XXX'
+OAUTH_TOKEN = 'XXX'
+OAUTH_TOKEN_SECRET = 'XXX'
+
+target = 'officialjaden'
+
+auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+t = twitter.Twitter(auth=auth)
+
+since = 1
+statuses = t.statuses.user_timeline(screen_name=target, contributor_details=False, 
+	trim_user=True, count = 200, include_rts=False)
+
+with open('jaydentweets.json', 'w') as outfile:
+	json.dump(statuses, outfile)
