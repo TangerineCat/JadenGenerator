1a7d6161812d0779f19341109f05326d62a7c80d
Kevin Tang <ktang@caltech.edu>
2015-02-19 06:35:23 -0800

    Initial Commit
    sublime commit thing didn't work :unamused:

---
+++ JaydenAnalyze.py
@@ -0,0 +1,22 @@
+import json
+from pymarkovchain import MarkovChain
+import re
+
+filename = 'jaydentweets.json'
+statuses = json.loads(open(filename).read())
+
+tweets = []
+
+for tweet in statuses:
+	tweets.append(tweet['text'].encode('ascii','ignore'))
+
+longstring = ''
+for tweet in tweets:
+	if len(tweet)>1:
+		insert = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', tweet)
+		longstring = longstring + "\n" + insert
+
+mc = MarkovChain()
+mc.generateDatabase(longstring, sentenceSep='[\n]')
+for x in xrange(1,10):
+	print(mc.generateString())
