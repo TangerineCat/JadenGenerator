2f3070cdb7f45ebd7d9a6eb57d0c547a9a9bec62
Kevin Tang <ktang@caltech.edu>
2015-02-19 08:05:42 -0800

    Ok, now I get all of his tweets
    And only store the text of his tweets as a json. Cut down space usage by
    at least 90%

--- JadenDownload.py
+++ JadenDownload.py
@@ -1,10 +1,12 @@
 import twitter
 import json
 
-CONSUMER_KEY = 'XXX'
-CONSUMER_SECRET = 'XXX'
-OAUTH_TOKEN = 'XXX'
-OAUTH_TOKEN_SECRET = 'XXX'
+CONSUMER_KEY = '6OnQ8zoMpPKzH0484ghUzEWJn'
+CONSUMER_SECRET = 'YKaFbn8s2F5kXd80bcvJx4eq6a6hbhZKoE9P8YgsLVbzP15f3r'
+OAUTH_TOKEN = '2935565371-CKEB82H7uIJMIE7QMsDm0vZLTiVzBkRNSeAM9lY'
+OAUTH_TOKEN_SECRET = 'zaR41Y81PHmWwfz4QsNKCFietda2cCMixY7bM0HqAIg5z'
+
+
 
 target = 'officialjaden'
 
@@ -14,7 +16,30 @@ t = twitter.Twitter(auth=auth)
 
 since = 1
 statuses = t.statuses.user_timeline(screen_name=target, contributor_details=False, 
-	trim_user=True, count = 200, include_rts=False)
+	trim_user=True, count = 200, include_rts=False, exclude_replies = True)
+
+newid = statuses[-1]['id']
+lastid = newid+1000000000
+
+while lastid > newid:
+	print(newid)
+	lastid = newid
+	new_statuses = t.statuses.user_timeline(screen_name=target, contributor_details=False, 
+		trim_user=True, count = 200, include_rts=False, exclude_replies = True, max_id = lastid)
+	try:
+		newid = new_statuses[-1]['id']
+		statuses += new_statuses
+	except:
+		print('reached end. what do?')
+		print(new_statuses)
+		break;
+
+texts = []
+for status in statuses:
+	dick = {}
+	dick['text'] = status['text']
+	texts.append(dick)
+
 
 with open('jadentweets.json', 'w') as outfile:
-	json.dump(statuses, outfile)
+	json.dump(texts, outfile)
